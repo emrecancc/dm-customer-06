d62013f947c72871d74280c32d1e3466a9db47d3
emrecancc <emrecancerli55@gmail.com>
2026-07-03 00:40:24 +0300

    fix: correct processor arrow function syntax

--- src/processor.js
+++ src/processor.js
@@ -1,6 +1,3 @@
-function processData(items) {
-  return items.map(item => {
-    return item.value * 2
-  }
+module.exports = (item) => {
+  return item.value * 2
 }
-module.exports = { processData };

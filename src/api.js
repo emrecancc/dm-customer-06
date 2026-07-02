26199558e13d3d46232d0fd624cbedaba0a08a82
emrecancc <emrecancerli55@gmail.com>
2026-07-02 07:30:38 +0300

    ci: introduce syn_js (s52)

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const handler = (req, res) => {
-  const data = await fetchData();
-  res.json(data);
-};
-module.exports = { handler };
+async function getUser(id {
+  const r = await fetch(`/api/users/${id}`);
+  return r.json();
+}
+module.exports = { getUser };
